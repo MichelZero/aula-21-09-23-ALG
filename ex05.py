9d410c2a7c5c5d553cef03705e714f0bd26ad539
Michel <106674127+MichelZero@users.noreply.github.com>
2023-09-21 04:43:00 -0300

    first commit

--- ex05.py
+++ ex05.py
@@ -3,3 +3,11 @@
 #
 # Data: 21/09/2023
 #
+# Dada uma tupla, crie uma nova tupla contendo os elementos a partir do terceiro 
+# elemento até o último. Imprima a nova tupla.
+# tupla = (10, 20, 30, 40, 50)
+
+
+tupla = (10, 20, 30, 40, 50)
+nova_tupla = tupla[2:]
+print("Nova tupla:", nova_tupla)
